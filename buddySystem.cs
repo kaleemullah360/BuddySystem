a626d494add897d0e1df31a52adef1e118a598ee
kaleemkhaleef <kaleemullah@khaleef.com>
2016-07-10 16:14:46 +0500

    updates

--- buddySystem.cs
+++ buddySystem.cs
@@ -28,9 +28,9 @@ namespace Buddy_system_space {
 
   class Buddy_system_class{
     static void Main(string []args){
-      Console.WriteLine("What should be the Size of our Buddy System ? (Must Integer)");
+      Console.WriteLine("What should be the Size of our Buddy System ? (Must be Integer)");
       int buddy_system_max_mem_size = Convert.ToInt32(Console.ReadLine());
-      Console.WriteLine("What should be the min chunk size ? (Must Integer)");
+      Console.WriteLine("What should be the min chunk size ? (Must be Integer)");
       int chunk_size = Convert.ToInt32(Console.ReadLine());
     }
 
